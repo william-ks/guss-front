5a7d083589427fa7775e3631dec285bf455a6680
William Krisley <will.ks2101@gmail.com>
2024-12-10 00:47:52 -0300

    add .env file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,12 @@ export default defineNuxtConfig({
     configPath: "~/tailwind.config.ts",
   },
 
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL,
+    },
+  },
+
   colorMode: {
     preference: "system", // default value of $colorMode.preference
     fallback: "light", // fallback value if not system preference found
